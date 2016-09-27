9aea0c5ec75fdf31b5ff6884ae6d53aafb93be7d
スズキリュウタロウ <szkrtr1992@yahoo.co.jp>
2016-09-27 12:10:07 +0900

    add: Enemy Spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour
+{
+  [SerializeField]
+  private GameObject enemyPrefab;
+  private float spawnDelay = 0.0f;
+  private float spawnInterval = 1.0f;
+
+  // Use this for initialization
+  void Start()
+  {
+    InvokeRepeating("Spawn", spawnDelay, spawnInterval);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  private void Spawn()
+  {
+    Instantiate(enemyPrefab, transform.position, transform.rotation);
+  }
+
+}
