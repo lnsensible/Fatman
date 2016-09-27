740a17a39ef5fc3835616043a0d1cc3398fee2dd
Ryutaro SUZUKI <szkrtr1992@yahoo.co.jp>
2016-09-27 18:27:56 +0900

    add: FoodScript have FoodPoint-Property. suppose it is used in FeverSystem. update: Food not destroyed by Enemy anymore. Food still sticks to Player.

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Food : MonoBehaviour
+{
+  [SerializeField]
+  private int foodPoint;
+  private PlayerScript player;
+
+  public int FoodPoint {
+    get { return foodPoint; }
+  }
+
+  // Use this for initialization
+  void Start()
+  {
+    player = FindObjectOfType<PlayerScript>();
+  }
+
+  void OnTriggerEnter(Collider col)
+  {
+    if (col.tag == "Player" && !player.holdingFood)
+    {
+      player.HoldFood(gameObject);
+      transform.SetParent(col.transform, true);
+      Destroy(GetComponent<LookAtCamera>());
+    }
+  }
+
+}
