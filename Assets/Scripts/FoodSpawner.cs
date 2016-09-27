afae4a3c7a9cd445854959e107269704df89cd2e
スズキリュウタロウ <szkrtr1992@yahoo.co.jp>
2016-09-27 15:27:11 +0900

    add: FoodSpawner can provide some kinds of Food. Always 3 Foods appear in field. Spawns at another location when collected by player. Respawn location is chosen from some fixed candidates.

---
+++ Assets/Scripts/FoodSpawner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodSpawner : MonoBehaviour
+{
+  [SerializeField]
+  private GameObject[] foodPrefabs;
+  [SerializeField]
+  private GameObject[] spawnPoints;
+
+  // Use this for initialization
+  void Start()
+  {
+    int count = 0;
+    while (count < 3)
+    {
+      var spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+      if (spawnPoint.transform.childCount == 0)
+      {
+        GameObject food = Instantiate(foodPrefabs[Random.Range(0, foodPrefabs.Length)], spawnPoint.transform.position, Quaternion.identity) as GameObject;
+        food.transform.SetParent(spawnPoint.transform);
+        ++count;
+      }
+    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    while (GameObject.FindGameObjectsWithTag("Food").Length < 3)
+    {
+      var spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+      if (spawnPoint.transform.childCount == 0)
+      {
+        GameObject food = Instantiate(foodPrefabs[Random.Range(0, foodPrefabs.Length)], spawnPoint.transform.position, Quaternion.identity) as GameObject;
+        food.transform.SetParent(spawnPoint.transform);
+      }
+    }
+  }
+}
