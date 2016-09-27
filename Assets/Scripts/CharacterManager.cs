401a328109c2b46eac26d456090bc49ef3caf2fa
lnsensible <crwengyew@gmail.com>
2016-09-27 19:55:40 +0900

    Changed screen smoke effect for fever Added fever gauge

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -1,20 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class CharacterManager : MonoBehaviour {
 
-    public float feverTime;
-    public int feverRequired;
+    public float feverTime = 5;
+    public float feverRequired = 10;
 
-    public float FOVSpeed;
-    public float FOVBackSpeed;
-    public float FeverFOV;
+    public float FOVSpeed = 100;
+    public float FOVBackSpeed = 20;
+    public float FeverFOV = 80;
     public float NormalFOV = 60;
+
+    public float MaxPulseScale = 0.7f;
+    public float PulseSpeed = 1.0f;
+    public float originalPulse = 0.5f;
+
+    public float fillSpeed;
+
+    public Image feverFill;
+    public GameObject feverGauge;
 
     [SerializeField]
     ParticleSystem[] FeverFX;
 
-    int feverAmount = 0;
+    float feverAmount = 0;
     bool inFeverMode = false;
     float feverTimer = 0.0f;
 
@@ -49,13 +59,53 @@ public class CharacterManager : MonoBehaviour {
             FeverFX[i].Stop();
             FeverFX[i].Clear();
         }
+
+        feverFill.fillAmount = 0;
+    }
+
+    IEnumerator GaugeFill()
+    {
+        float fillpercentage = feverAmount/feverRequired;
+        if (fillpercentage > 1.0f)
+            fillpercentage = 1.0f;
+
+        while (feverFill.fillAmount < fillpercentage)
+        {
+            yield return null;
+            feverFill.fillAmount = Mathf.MoveTowards(feverFill.fillAmount, fillpercentage, Time.deltaTime * fillSpeed);
+        }
+
+        if (fillpercentage == 1.0f)
+        {
+            FEVER();
+        }
+    }
+
+    IEnumerator Deplete()
+    {
+        float max = 1;
+        float min = 0;
+        float t = 0;
+        while (feverFill.fillAmount > 0)
+        {
+            yield return null;
+            t += Time.deltaTime/feverTime;
+            feverFill.fillAmount = Mathf.MoveTowards(max, min, t);
+        }
+    }
+
+    public void EatFood(float amt)
+    {
+        feverAmount += amt;
+        StopCoroutine("GaugeFill");
+        StartCoroutine("GaugeFill");
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            FEVER();
+            EatFood(1.0f);
         }
 
         if (inFeverMode)
@@ -80,6 +130,8 @@ public class CharacterManager : MonoBehaviour {
         {
             FeverFX[i].Play();
         }
+        StartCoroutine("Deplete");
+        StartCoroutine("Pulse");
     }
 
     public void UNFEVER()
@@ -110,6 +162,21 @@ public class CharacterManager : MonoBehaviour {
                 yield return null;
                 Camera.main.fieldOfView -= Time.deltaTime * FOVBackSpeed;
             }
+        }
+    }
+
+    IEnumerator Pulse()
+    {
+        Vector3 scaleSpeed = new Vector3(PulseSpeed, PulseSpeed, 0);
+        Vector3 normalScale = new Vector3(originalPulse, originalPulse, 1);
+        while (true)
+        {
+            feverGauge.transform.localScale += scaleSpeed * Time.deltaTime;
+            if (feverGauge.transform.localScale.x > MaxPulseScale)
+            {
+                feverGauge.transform.localScale = normalScale;
+            }
+            yield return null;
         }
     }
 }
