d2195ef14d292ba5285c75044c670da6582aa50d
TSITCL\s152099 <masayasu1993@gmail.com>
2016-09-27 12:14:42 +0900

    プレイヤー成長

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,14 +2,23 @@
 using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
-
+    //!食べているかどうか
     public bool holdingFood;
-
+    //!食べている時間
     private float eatingTimer;
 
     public GameObject foodHeld;
 
     HungerManager hunger;
+
+    //!成長するまで何個食べたか
+    private int ate = 0;
+    //!太るまでの上限
+    private int hungerUpperLimit = 1;
+    //!デブレベル
+    public int fatLevel = 1;
+
+
 	// Use this for initialization
 	void Start () {
         hunger = FindObjectOfType<HungerManager>();
@@ -17,16 +26,21 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    if (holdingFood)
+        PlayerAttack();
+        //!食べているとき
+        if (holdingFood)
         {
             eatingTimer -= Time.deltaTime;
+            //!食べ終わったら
             if (eatingTimer < 0)
             {
+                ate += 1;
                 hunger.Eat();
                 Destroy(foodHeld);
                 GetComponent<Rigidbody>().mass += 1;
-                StopCoroutine("Grow");
-                StartCoroutine("Grow");
+                SatisfyStomach();
+                //StopCoroutine("Grow");
+                //StartCoroutine("Grow");
                 holdingFood = false;
             }
         }
@@ -35,7 +49,7 @@ public class PlayerScript : MonoBehaviour {
     IEnumerator Grow()
     {
         float growthleft = 1.0f;
-        float growthspeed = 0.2f / growthleft;
+        float growthspeed = 0.5f / growthleft;
         while (growthleft > 0.0f)
         {
             yield return null;
@@ -73,4 +87,28 @@ public class PlayerScript : MonoBehaviour {
             hunger.Damaged();
         }
     }
+
+    void PlayerAttack()
+    {
+            if(Input.GetButtonDown("Jump"))
+            {
+                this.transform.Rotate(20.0f,0.0f,0.0f);
+            }
+    }
+
+    ///
+    ///@    食べた数が成長までの上限を超えたら大きくする
+    ///@    デブレベルをワンランク上げて成長までにかかる量を増やす
+    public void SatisfyStomach()
+    {
+        if(ate > hungerUpperLimit)
+        {
+            StopCoroutine("Grow");
+            StartCoroutine("Grow");
+            ate = 0;
+            fatLevel += 1;
+            hungerUpperLimit += 2;
+        }
+    } 
+
 }
