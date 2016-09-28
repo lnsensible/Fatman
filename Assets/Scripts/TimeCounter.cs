080e81cd70fa3794cdffe95094ea33b40a97ea45
スズキリュウタロウ <szkrtr1992@yahoo.co.jp>
2016-09-28 16:26:33 +0900

    add: TimeCounter has elapsed time from start as float. use ElapsedTime-Property to get it.

---
+++ Assets/Scripts/TimeCounter.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeCounter : MonoBehaviour
+{
+  private TitleScreen titleScreen;
+  private float startingTime;
+  [SerializeField]
+  private float elapsedTimeFromStarting;
+  private Text time;
+
+  public float ElapsedTime
+  {
+    get { return elapsedTimeFromStarting; }
+  }
+
+  void Start()
+  {
+    titleScreen = FindObjectOfType<TitleScreen>();
+    time = GetComponent<Text>();
+  }
+
+  void Update()
+  {
+    if (titleScreen.inTitle == true)
+    {
+      startingTime = Time.time;
+    }
+    else
+    {
+      elapsedTimeFromStarting = Time.time - startingTime;
+      int min = (int)(ElapsedTime / 60) % 60;
+      int sec = (int)(ElapsedTime) % 60;
+      int msec = (int)(ElapsedTime * 100) % 100;
+      time.text = min.ToString("D2") + ":" + sec.ToString("D2") + ":" + msec.ToString("D2");
+    }
+  }
+
+}
