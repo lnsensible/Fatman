d77cc3809935308454d1281e3d4ff0de043aa49e
TSITCL\s152099 <masayasu1993@gmail.com>
2016-09-28 09:55:35 +0900

    level_growth

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -14,20 +14,27 @@ public class PlayerScript : MonoBehaviour {
     //!成長するまで何個食べたか
     private int ate = 0;
     //!太るまでの上限
-    private int hungerUpperLimit = 1;
+    private int hungerUpperLimit = 4;
     //!デブレベル
     public int fatLevel = 1;
+    //!生存フラグ
+    //public bool dead = false;
 
 
 	// Use this for initialization
 	void Start () {
         hunger = FindObjectOfType<HungerManager>();
-    reference = this;
+    //reference = this;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        PlayerAttack();
+        //!死んでいるかどうか
+        //if(dead)
+        //{
+        //    //!移動速度を0に
+        //    this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        //}
         //!食べているとき
         if (holdingFood)
         {
@@ -62,7 +69,7 @@ public class PlayerScript : MonoBehaviour {
     public void HoldFood(GameObject food)
     {
         foodHeld = food;
-        eatingTimer = 1.5f;
+        eatingTimer = 0.1f;
         holdingFood = true;
     }
 
@@ -75,41 +82,39 @@ public class PlayerScript : MonoBehaviour {
         holdingFood = false;
     }
 
+    //!ほかのオブジェクトとの衝突
     void OnCollisionEnter(Collision col)
     {
+        //!エネミーと衝突したら
         if (col.transform.tag == "Enemy")
         {
-            Debug.Log("enemy");
-            Vector3 dir = col.contacts[0].point - transform.position;
-            dir = -dir.normalized;
-            GetComponent<CharacterMovement>().Knocked();
-            GetComponent<Rigidbody>().velocity = (dir * 5.0f);
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), 0.5f);
-            hunger.Damaged();
+            //dead = true;
+            //Destroy(this.gameObject);
+            //Debug.Log("enemy");
+            //Vector3 dir = col.contacts[0].point - transform.position;
+            //dir = -dir.normalized;
+            //GetComponent<CharacterMovement>().Knocked();
+            //GetComponent<Rigidbody>().velocity = (dir * 5.0f);
+            //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), 0.5f);
+            //hunger.Damaged();
         }
-    }
-
-    void PlayerAttack()
-    {
-            if(Input.GetButtonDown("Jump"))
-            {
-                this.transform.Rotate(20.0f,0.0f,0.0f);
-            }
     }
 
     ///
     ///@    食べた数が成長までの上限を超えたら大きくする
-    ///@    デブレベルをワンランク上げて成長までにかかる量を増やす
+    ///@    デブレベルをワンランク上げた後成長までにかかる量を増やす
+    ///
     public void SatisfyStomach()
     {
-        if(ate > hungerUpperLimit)
+        if(ate >= hungerUpperLimit)
         {
             StopCoroutine("Grow");
             StartCoroutine("Grow");
             ate = 0;
             fatLevel += 1;
-            hungerUpperLimit += 2;
+            hungerUpperLimit *= fatLevel;
         }
-    } 
+    }
+
 
 }
